92573576962c3fdd5bef827fd991a6465233a623
Gerard de Leeuw <gdeleeuw@leeuwit.nl>
2022-06-16 13:38:44 +0200

    Issue a self-signed certificate while waiting for the ACME server

--- src/main/kotlin/com/github/lion7/ktor/server/acme/AcmeConnector.kt
+++ src/main/kotlin/com/github/lion7/ktor/server/acme/AcmeConnector.kt
@@ -1,9 +1,12 @@
 package com.github.lion7.ktor.server.acme
 
-import io.ktor.server.engine.*
-import io.ktor.server.jetty.*
+import io.ktor.server.engine.ConnectorType
+import io.ktor.server.engine.EngineConnectorBuilder
+import io.ktor.server.engine.EngineSSLConnectorConfig
+import io.ktor.server.jetty.JettyApplicationEngineBase
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
+import org.bouncycastle.pkcs.PKCS10CertificationRequest
 import org.shredzone.acme4j.Authorization
 import org.shredzone.acme4j.Certificate
 import org.shredzone.acme4j.Identifier
@@ -13,6 +16,7 @@ import org.shredzone.acme4j.challenge.TlsAlpn01Challenge
 import org.shredzone.acme4j.util.CSRBuilder
 import org.shredzone.acme4j.util.CertificateUtils
 import org.shredzone.acme4j.util.KeyPairUtils
+import org.slf4j.LoggerFactory
 import java.io.File
 import java.security.KeyPair
 import java.security.KeyStore
@@ -71,11 +75,20 @@ class AcmeConnector(
     internal val certificate: X509Certificate?
         get() = keyStore.getCertificate(keyAlias) as? X509Certificate
 
+    private val log = LoggerFactory.getLogger(javaClass)
     private val threadFactory: ThreadFactory = object : ThreadFactory {
         override fun newThread(r: Runnable): Thread = thread(start = false, isDaemon = true, name = javaClass.simpleName) { r.run() }
     }
     private val executor = Executors.newSingleThreadScheduledExecutor(threadFactory)
     private val sslReloader = JettySslReloader()
+    private val selfSignedIssuer by lazy {
+        CertificateUtils.createTestRootCertificate(
+            "CN=Ktor Acme Connector",
+            Instant.parse("2000-01-01T00:00:00Z"),
+            Instant.parse("2100-01-01T00:00:00Z"),
+            KeyPairUtils.createKeyPair(2048)
+        )
+    }
 
     val configure: JettyApplicationEngineBase.Configuration.() -> Unit = {
         configureServer = {
@@ -89,39 +102,41 @@ class AcmeConnector(
 
     private fun orderCertificate() {
         val currentCertificate = certificate
-        if (currentCertificate == null || currentCertificate.notAfter.toInstant().isBefore(Instant.now().minus(Duration.ofDays(7)))) {
-            val account = accountManager.getOrCreateAccount()
-            val order = account.newOrder().domain(domain).create()
-            val authorization = order.authorizations.first { it.identifier.domain == domain }
+        try {
+            if (currentCertificate == null || currentCertificate.notAfter.toInstant().isBefore(Instant.now().minus(Duration.ofDays(7)))) {
+                val account = accountManager.getOrCreateAccount()
+                val order = account.newOrder().domain(domain).create()
+                val authorization = order.authorizations.first { it.identifier.domain == domain }
 
-            if (authorization.status == Status.PENDING) {
-                executeTlsChallenge(authorization)
+                if (authorization.status == Status.PENDING) {
+                    executeTlsChallenge(authorization)
+                }
+
+                order.update()
+
+                if (order.status == Status.READY) {
+                    val existingPrivateKey = keyStore.getKey(keyAlias, privateKeyPassword()) as? PrivateKey
+                    val keyPair = if (currentCertificate != null && existingPrivateKey != null) {
+                        KeyPair(currentCertificate.publicKey, existingPrivateKey)
+                    } else {
+                        KeyPairUtils.createKeyPair(2048)
+                    }
+                    val certificate = requestCertificate(order, keyPair) ?: throw IllegalStateException("Certificate is not available")
+
+                    // add the requested certificate to the keystore
+                    persistCertificateChain(keyPair.private, certificate.certificateChain)
+                }
             }
-
-            order.update()
-
-            if (order.status == Status.READY) {
-                val existingPrivateKey = keyStore.getKey(keyAlias, privateKeyPassword()) as? PrivateKey
-                val keyPair = if (currentCertificate != null && existingPrivateKey != null) {
-                    KeyPair(currentCertificate.publicKey, existingPrivateKey)
-                } else {
-                    KeyPairUtils.createKeyPair(2048)
+        } catch (e: Exception) {
+            log.error("Failed to order certificate, retrying in 1 minute", e)
+            executor.schedule(this::orderCertificate, 1, TimeUnit.MINUTES)
+            // Generate a self-signed certificate while waiting on ACME
+            if (certificate == null) {
+                try {
+                    createSelfSignedCertificate()
+                } catch (e: Exception) {
+                    log.error("Failed to generate self-signed certificate", e)
                 }
-                val certificate = requestCertificate(order, keyPair)
-                if (certificate == null) {
-                    println("Certificate is not available") // TODO: log
-                    return
-                }
-
-                // add the requested certificate to the keystore
-                val chain = certificate.certificateChain.toTypedArray()
-                keyStore.setKeyEntry(keyAlias, keyPair.private, privateKeyPassword(), chain)
-
-                // persist the updated keystore to disk
-                keyStorePath.outputStream().use { keyStore.store(it, keyStorePassword()) }
-
-                // reload the Jetty SSL context factories
-                sslReloader.reloadSslContextFactories()
             }
         }
     }
@@ -129,7 +144,7 @@ class AcmeConnector(
     private fun executeTlsChallenge(authorization: Authorization) {
         val challenge = authorization.findChallenge(TlsAlpn01Challenge::class.java)
         if (challenge == null) {
-            println("TLS ALPN 01 challenge is not available") // TODO: log
+            log.error("TLS ALPN 01 challenge is not available")
             return
         }
 
@@ -183,6 +198,37 @@ class AcmeConnector(
         }
 
         return null
+    }
+
+    private fun createSelfSignedCertificate() {
+        val keyPair = KeyPairUtils.createKeyPair(2048)
+        val certificateSigningRequest = CSRBuilder().apply {
+            addDomain(domain)
+            sign(keyPair)
+        }.encoded
+        val certificate = CertificateUtils.createTestCertificate(
+            PKCS10CertificationRequest(certificateSigningRequest),
+            Instant.parse("2000-01-01T00:00:00Z"),
+            Instant.parse("2100-01-01T00:00:00Z"),
+            selfSignedIssuer,
+            keyPair.private
+        )
+
+        // add the self-signed certificate to the keystore
+        persistCertificateChain(keyPair.private, listOf(certificate, selfSignedIssuer))
+
+        log.info("Generated self-signed certificate while waiting on ACME-issued certificate")
+    }
+
+    private fun persistCertificateChain(privateKey: PrivateKey, chain: List<X509Certificate>) {
+        // add the certificate to the keystore
+        keyStore.setKeyEntry(keyAlias, privateKey, privateKeyPassword(), chain.toTypedArray())
+
+        // persist the updated keystore to disk
+        keyStorePath.outputStream().use { keyStore.store(it, keyStorePassword()) }
+
+        // reload the Jetty SSL context factories
+        sslReloader.reloadSslContextFactories()
     }
 
 }
